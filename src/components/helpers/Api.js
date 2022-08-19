95eb4309ff0e22aecb470bce876f836beca095d0
BiahLages <biahlages@gmail.com>
2022-08-19 17:37:04 -0300

    att api

--- src/components/helpers/Api.js
+++ src/components/helpers/Api.js
@@ -4,7 +4,12 @@ const CatContext = {
   catById: (id) => `${CatContext.catEndpoint()}/${id}`,
 };
 
+const urls = {
+  development: "http://localhost:8000",
+  production: "https://fake-api-cat.herokuapp.com/"
+}
+
 export const Api = {
-  baseUrl: "http://localhost:8000",
+  baseUrl: urls[process.env.NODE_ENV],
   ...CatContext,
 };
